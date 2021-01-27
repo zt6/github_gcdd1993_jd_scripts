2794631bb85fb10a0281fcf2965838ba9726f180
Gcdd1993 <mobile17602526128@gmail.com>
2021-01-27 14:21:32 +0800

    try fix git pull error

--- docker/docker_entrypoint.sh
+++ docker/docker_entrypoint.sh
@@ -12,8 +12,6 @@ fi
 echo "##############################################################################"
 echo "Container start , Pull the latest code..."
 echo "容器启动，git 拉取最新代码..."
-git remote set-url origin https://github.com/gcdd1993/jd_scripts
-git branch --set-upstream-to=origin/master master
 git reset --hard
 git -C /scripts pull
 npm install --prefix /scripts
