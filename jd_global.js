ef9b930c326d905737bb7eb9eccafcaecdf2dfdf
Gcdd1993 <mobile17602526128@gmail.com>
2021-02-10 18:55:52 +0800

    修改为自己的助力码

--- jd_global.js
+++ jd_global.js
@@ -50,8 +50,16 @@ if ($.isNode()) {
 
 const JD_API_HOST = 'https://api.m.jd.com/', actCode = 'visa-card-001';
 const inviteCodes = [
-  'WmpHM2pndWh3OFphS2NsbTRLMmhqZz09@M3ozUGw0eExUZ25hSHBTZ2pJcTdpZz09@S2tETnZ0REtONy9Dc2Nqek1KNXpmWHFTNnF3OUtQQjJKZmJ2YUtSS3BQTT0=@a1RrenU1WExQaXRWS3VIZHgwMjlUYzJSeHhVMDlvZXgxR2RsdkZkRXZnOD0=@bHNsOVFIL2tQRTJhSndpRVNHVTlheXJLbzZRK09HaUtidjJUUFNQRXdqbz0=@M0JxTFVEbmxtV05uQWJVQVdyL2NxeTcycG1lcWtEbzVOc283bjR2MklkWT0=',
-  'a1RrenU1WExQaXRWS3VIZHgwMjlUYzJSeHhVMDlvZXgxR2RsdkZkRXZnOD0=@bHNsOVFIL2tQRTJhSndpRVNHVTlheXJLbzZRK09HaUtidjJUUFNQRXdqbz0=@WmpHM2pndWh3OFphS2NsbTRLMmhqZz09@M3ozUGw0eExUZ25hSHBTZ2pJcTdpZz09@S2tETnZ0REtONy9Dc2Nqek1KNXpmWHFTNnF3OUtQQjJKZmJ2YUtSS3BQTT0=',
+  'bjBURTJlREFCTFpBRXF1UzFaME1qaU5KOUNteXVLYVJZOEV6SXRLR3dXND0=@' +
+  'c2I5TE1HeFJMNFlFSHk1Q0p3TVllUT09@' +
+  'SGwxdHIvZnNlemp1ZFJBcHRKWTZLYTVBekFjR2IvaTRGNkZpUzdWOFIzQT0=@' +
+  'dUY4aHZTZVpYbVpoMWJrMnVweHVKTlFYZXY4Uk1OTXdOblIwOVhTaTJTQT0=@' +
+  'bHljd3FUODdISHBFS0xRWDl5TjJpUT09@' +
+  'OWlyM1ZiUWJncXQrZ1VMZTU1Sm80RWFsUUhiak9aUitERDlUQWpBWkhXaz0=@' +
+  'cEJ4dFQvZVV1TmYzTTRUY3Y4TWx0QT09@' +
+  'WUVmMlBZcDdXTUphRDdOWm5VVklMK0FpODdqUC9idFVCOEIzWjNQTExiaz0=@' +
+  'cjdqT0J2TVJxL1AvemhMbG1SRTE4b3ZMSE1abXh6dWh4eUcxYlpnYXRpcz0=@' +
+  'NXpWK2ZrTm1MQlo2MzRLN0REZnd1ZllJTXJjNlRCVWhJaHd4SHZUTWR4dz0=@'
 ];
 $.invites = [];
 !(async () => {
@@ -349,18 +357,7 @@ function readShareCode() {
 function shareCodesFormat() {
   return new Promise(async resolve => {
     // console.log(`第${$.index}个京东账号的助力码:::${$.shareCodesArr[$.index - 1]}`)
-    $.newShareCodes = [];
-    if ($.shareCodesArr[$.index - 1]) {
-      $.newShareCodes = $.shareCodesArr[$.index - 1].split('@');
-    } else {
-      console.log(`由于您第${$.index}个京东账号未提供shareCode,将采纳本脚本自带的助力码\n`)
-      const tempIndex = $.index > inviteCodes.length ? (inviteCodes.length - 1) : ($.index - 1);
-      $.newShareCodes = inviteCodes[tempIndex].split('@');
-    }
-    const readShareCodeRes = await readShareCode();
-    if (readShareCodeRes && readShareCodeRes.code === 200) {
-      $.newShareCodes = [...new Set([...$.newShareCodes, ...(readShareCodeRes.data || [])])];
-    }
+    $.newShareCodes = inviteCodes[0].split('@');
     console.log(`第${$.index}个京东账号将要助力的好友${JSON.stringify($.newShareCodes)}`)
     resolve();
   })
