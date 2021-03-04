ea26b85e0ad3feb256d4dc4ed6ddbb1a732e2d47
Gcdd1993 <mobile17602526128@gmail.com>
2021-03-04 10:14:30 +0800

    change docker registry

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 VERSION=0.1.5
 REGISTRY=registry.cn-shanghai.aliyuncs.com
-NAME=gcdd1993/jd_scripts
+NAME=halmawork/jd_scripts
 docker build . -t ${REGISTRY}/${NAME}:${VERSION}
 docker push ${REGISTRY}/${NAME}:${VERSION}
