51e9d10a215a54baa9453b0d84bab381901c9072
gcdd1993 <gcwm99@gmail.com>
2021-06-05 21:37:44 +0800

    升级依赖 & 优化Dockerfile

--- docker/docker_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-VERSION=0.1.5
-REGISTRY=registry.cn-shanghai.aliyuncs.com
-NAME=halmawork/jd_scripts
-docker build . -t ${REGISTRY}/${NAME}:${VERSION}
-docker push ${REGISTRY}/${NAME}:${VERSION}
