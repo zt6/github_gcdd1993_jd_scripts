fe6ed358fab5c3eaf44829c19eb384abed2b8c8f
gcdd1993 <gcwm99@gmail.com>
2021-06-22 17:34:10 +0800

    fix crontab

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -26,8 +26,6 @@
 0 1,21 * * * node /scripts/jd_star_shop.js >> /scripts/logs/jd_star_shop.log 2>&1
 #新潮品牌狂欢（6.18过期）
 20 1,21 * * * node /scripts/jd_mcxhd.js >> /scripts/logs/jd_mcxhd.log 2>&1
-#京喜领88元红包(6.31到期)
-30 1,6,12,21 * * * node /scripts/jd_jxlhb.js >> /scripts/logs/jd_jxlhb.log 2>&1
 #省钱大赢家之翻翻乐
 10,40 * * * * node /scripts/jd_big_winner.js >> /scripts/logs/jd_big_winner.log 2>&1
 # 618大势新品赏
